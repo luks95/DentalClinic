3aeb9a48118980e903fb8450790dd0faba9b2c26
Lucas <hake.lu@hotmail.com>
2017-11-13 16:44:36 -0300

    Modificacion del Formulario de Producto

--- DentalClinic/src/consultorio/swing/abm/FormularioProducto.java
+++ DentalClinic/src/consultorio/swing/abm/FormularioProducto.java
@@ -7,6 +7,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Font;
 import javax.swing.JLabel;
+import consultorio.swing.componentes.TfGeneric;
 
 public class FormularioProducto extends JDialog {
 
@@ -31,8 +32,10 @@ public class FormularioProducto extends JDialog {
 
 	/**
 	 * Create the dialog.
+	 *
 	 */
 	public FormularioProducto() {
+		
 		setBounds(100, 100, 330, 425);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -44,14 +47,41 @@ public class FormularioProducto extends JDialog {
 		button.setBounds(200, 11, 104, 48);
 		contentPanel.add(button);
 		
-		JLabel label = new JLabel("");
-		label.setFont(new Font("Tahoma", Font.BOLD, 15));
-		label.setBounds(99, 11, 92, 24);
-		contentPanel.add(label);
+		JLabel lblCodigo = new JLabel("");
+		lblCodigo.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblCodigo.setBounds(99, 11, 92, 24);
+		contentPanel.add(lblCodigo);
 		
-		JLabel label_1 = new JLabel("Nro. Servicio");
-		label_1.setFont(new Font("Century Gothic", Font.PLAIN, 11));
-		label_1.setBounds(10, 11, 68, 24);
-		contentPanel.add(label_1);
+		JLabel lblCdigo = new JLabel("C\u00F3digo");
+		lblCdigo.setFont(new Font("Century Gothic", Font.PLAIN, 11));
+		lblCdigo.setBounds(10, 11, 68, 24);
+		contentPanel.add(lblCdigo);
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setFont(new Font("Century Gothic", Font.PLAIN, 11));
+		lblNombre.setBounds(10, 67, 68, 24);
+		contentPanel.add(lblNombre);
+		
+		JLabel lblCaracteristicas = new JLabel("Caracteristicas");
+		lblCaracteristicas.setFont(new Font("Century Gothic", Font.PLAIN, 11));
+		lblCaracteristicas.setBounds(10, 135, 80, 24);
+		contentPanel.add(lblCaracteristicas);
+		
+		JLabel lblPrecioDeCosto = new JLabel("Precio de Costo");
+		lblPrecioDeCosto.setFont(new Font("Century Gothic", Font.PLAIN, 11));
+		lblPrecioDeCosto.setBounds(10, 203, 85, 24);
+		contentPanel.add(lblPrecioDeCosto);
+		
+		TfGeneric tfNombre = new TfGeneric();
+		tfNombre.setBounds(99, 70, 113, 22);
+		contentPanel.add(tfNombre);
+		
+		TfGeneric tfCaracteristicas = new TfGeneric();
+		tfCaracteristicas.setBounds(99, 137, 205, 22);
+		contentPanel.add(tfCaracteristicas);
+		
+		TfGeneric tfPrecioCosto = new TfGeneric();
+		tfPrecioCosto.setBounds(99, 205, 113, 22);
+		contentPanel.add(tfPrecioCosto);
 	}
 }
